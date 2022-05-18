ca3dbfa21edab049491e965372b07f3c1be67c7f
Matt Decker <matt@Matts-MBP.lan>
2022-05-18 17:49:17 -0400

    complete + stretch

--- index.js
+++ index.js
@@ -15,10 +15,23 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.stomach = []
+  this.name = name;
+  this.age = age;
 }
 
+Person.prototype.eat = function(someFood){
+  return (this.stomach.length < 10) ? this.stomach.push(someFood) : NaN;
+}
+
+Person.prototype.poop = function() {
+  return this.stomach = [];
+}
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
+}
 
 /*
   TASK 2
@@ -36,11 +49,28 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.tank = 0;
+  this.odometer = 0;
+  this.model = model;
+  this.milesPerGallon = milesPerGallon
 }
 
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+}
 
+Car.prototype.drive = function(distance) {
+  this.odometer += distance;
+  this.tank -= distance / this.milesPerGallon;
+  if (this.tank === 0) {
+    return `I ran out of fuel at ${this.odometer}, miles!`
+  }
+}
+const civic = new Car('civic', 10)
+civic.fill(20);
+civic.drive(50);
+//console.log(civic)
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -49,18 +79,22 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age, favoriteToy); 
+  this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window binding: not bound to anything will return window
+  2. Implicit binding: whats to the left of the dot when functions invoked
+  3. Explicit binding: .call, .bind, .apply
+  4. new binding: a new version of constructor function 
 */
 
 ///////// END OF CHALLENGE /////////
